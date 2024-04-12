c95bca50d467393d4d1f160577b5165bc36cedbf
Yuri Sanches Guedes <yuri.guedes2002@gmail.com>
2024-04-12 17:27:45 -0300

    espaco ajustado

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 def validarmes(x,y=''):
         if x[0] == 'J':
             print('PRIMEIRO MES DO ANO')
-        elif x[-1]== 'J':
+        elif x[-1] == 'J':
             print('Estamos no mes de festa junina ze ruela ')
 
 
